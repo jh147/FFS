55241efc4f4f6242f5bb73c4078dc07203620e2c
JoyceStone <cstbuctlz@163.com>
2017-12-31 14:11:09 +0800

    代理人列表

--- backend/views/agent/index.php
+++ backend/views/agent/index.php
@@ -9,7 +9,7 @@ $this->title = '代理人';
     <ul class="search-con clearfix">
         <li>
             <span>始发站</span>
-            <input type="text" class="form-control" placeholder="" style="width:200px;">
+            <input type="text" class="form-control" placeholder="" style="width:200px;" id="start_station">
         </li>
         <li>
             <span>&nbsp;</span>
@@ -28,7 +28,7 @@ $this->title = '代理人';
 
         </div>
         
-        <div class="grid-content" id="grid">
+        <div class="grid-content" id="agent_grid">
             <table class="table form">
                 <thead>
                   <tr>
@@ -39,6 +39,7 @@ $this->title = '代理人';
                     <th>代理人名称</th>
                     <th>经理</th>
                     <th>经理电话</th>
+                    <th>操作</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -47,29 +48,21 @@ $this->title = '代理人';
         </div>
     </div>
     
-    <script type="text/template" id="gridrow_template">
-        <td class="align-r">
-            <label class="form-checkbox" for="checkbox_<%- i %>">
-                <i class="icon-checkbox"></i>
-                <span class="align-m"></span>
-                <input type="checkbox" class="form-checkbox-input" id="checkbox_<%- i %>">
-            </label>
-        </td>
+    <script type="text/template" id="agent_grid_template">
         <td class="align-c"><%- i %></td>
-        <td><%- weixin %></td>
-        <td allowedit="name_template"><%- name %></td>
-        <td class="color-gray"><%- card %></td>
-        <td class="color-gray"><%- mobile %></td>
         <td>
-            <p><a href="/index.php?r=index/form">编辑</a></p>
-            <p><a href="javascript:;" class="del">删除</a></p>
+            <%- type %>
         </td>
-    </script>
-    
-    <script type="text/template" id="name_template">
-        <div class="form form-base">
-            <input type="text" class="form-control" value="<%- name%>" id="newname">
-        </div>
+        <td><%- name %></td>
+        <td><%- proj_name %></td>
+        <td><%- file_name %></td>
+        <td><%- upload_time %></td>
+        <td><%- file_size %></td>
+        <td>
+            <p><a class="edit" href="javascript:;" data-id="<%- id%>">修改</a></p>
+
+            <p><a class="btn-del" href="javascript:;" data-id="<%- id%>">删除</a></p>
+        </td>
     </script>
     <script type="text/javascript">
         seajs.use('/js/agent/agent-index.js');
